6e7b579099ec09410c7ba386b2524cb7dc3f1165
Lucas Reis <luk.wik40@gmail.com>
2023-07-06 22:42:50 -0300

    Criado os componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Formulario from "./components/Formulario";
+import Lista from "./components/Lista";
 
 function App() {
-  return <div className="App"> </div>;
+  return (
+    <div className="App">
+      <Formulario />
+      <Lista />
+    </div>
+  );
 }
 
 export default App;
