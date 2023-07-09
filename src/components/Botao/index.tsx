9648dd9706a4c15f771aa21ad1d64d7057020fe4
Lucas Reis <luk.wik40@gmail.com>
2023-07-09 19:27:20 -0300

    Implementado a estilização com SASS e CSS Modules

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import style from "./Botao.module.scss";
 
 class Botao extends React.Component {
   render() {
-    return <button>Botão</button>;
+    return <button className={style.botao}>Botão</button>;
   }
 }
 
